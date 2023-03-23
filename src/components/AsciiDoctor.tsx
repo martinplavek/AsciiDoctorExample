c30885ee53a860594fb1025d60517aa75412fcb2
Martin Plavek <martin.plavek@ataccama.com>
2023-03-23 15:04:48 +0100

    Render HTML in iframe and handle navigation

--- src/components/AsciiDoctor.tsx
+++ src/components/AsciiDoctor.tsx
@@ -1,11 +1,45 @@
-import React from "react";
-import { readAsciiDocFile } from "../utils/asciiDocFileReader";
-import { Parser } from 'html-to-react'
-
+import React, { useEffect, useRef } from 'react'
+import { readAsciiDocFile, readAsciiDocFileNext } from '../utils/asciiDocFileReader'
 export const AsciiDoctor = () => {
-    const parser = new Parser()
+  const iFrameRef = useRef<HTMLIFrameElement>(null)
 
-  const htmlContent = readAsciiDocFile()
-    console.log('htmlContent', htmlContent)
-  return <>{parser.parse(htmlContent)}</>
-};
+  function readFile(url: string) {
+      return url === 'aDocNext' ? readAsciiDocFileNext() : readAsciiDocFile()
+  }
+
+  function clickHandler(e: MouseEvent) {
+    e.preventDefault()
+    const href = (e.target as HTMLAnchorElement).href.split('/').pop()
+    console.log('target', e.target)
+    console.log('href', href)
+    readFile(href ?? 'aDoc')
+
+    if (iFrameRef.current?.contentDocument) {
+      const iFrameDocument = iFrameRef.current.contentDocument
+      iFrameDocument.open()
+      iFrameDocument.write(readFile(href ?? 'aDoc').toString())
+      iFrameDocument.addEventListener('click', clickHandler)
+      iFrameDocument.close()
+    }
+  }
+
+  useEffect(() => {
+    const iFrame = iFrameRef.current
+    iFrame?.addEventListener('click', (e) => {
+      e.preventDefault()
+      window.postMessage('clicked')
+      console.log('click event', e)
+    })
+    if (iFrame?.contentDocument) {
+      const iFrameDocument = iFrame.contentDocument
+      const content = readAsciiDocFile()
+      console.log('content', content)
+      iFrameDocument.open()
+      iFrameDocument.write(readAsciiDocFile().toString())
+      iFrameDocument.addEventListener('click', clickHandler)
+      iFrameDocument.close()
+    }
+  }, [])
+
+  return <iframe title="x" ref={iFrameRef} />
+}
--- src/utils/asciiDocFileReader.ts
+++ src/utils/asciiDocFileReader.ts
@@ -1,14 +1,19 @@
 import Processor from "asciidoctor";
 import asciidoctorHtml5s from "asciidoctor-html5s";
 
-const aDoc = '== Hello, AsciiDoc! \n This is an interactive editor. \n Use it to try https://asciidoc.org[AsciiDoc].'
-
+const aDoc = "== Hello, AsciiDoc!\nThis is an interactive editor. \n Use it to try https://asciidoc.org[AsciiDoc]. \n link:aDocNext[Next]"
+const aDocNext = "== Hello, AsciiDoc next!\nThis is an interactive editor. \n Use it to try https://asciidoc.org[AsciiDoc]. \n link:aDoc[Previous]"
 
 export function readAsciiDocFile()  {
     const processor = Processor();
-
-    console.log('aDoc', aDoc)
     asciidoctorHtml5s.register();
 
-    return processor.convert(aDoc, {standalone: false, backend: 'html5s'});
+    return processor.convert(aDoc, {backend: 'html5s', safe: 'unsafe'});
+}
+
+export function readAsciiDocFileNext()  {
+    const processor = Processor();
+    asciidoctorHtml5s.register();
+
+    return processor.convert(aDocNext, {backend: 'html5s', safe: 'unsafe'});
 }
